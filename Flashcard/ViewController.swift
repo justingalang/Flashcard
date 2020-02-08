a6a99bca39a9636a43f0852ede6c72bea85d3609
Justin Galang <justingalang@gmail.com>
2020-02-08 13:38:59 -0600

    Initial Commit

---
+++ Flashcard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcard
+//
+//  Created by Justin Galang on 2/8/20.
+//  Copyright © 2020 Justin Galang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
